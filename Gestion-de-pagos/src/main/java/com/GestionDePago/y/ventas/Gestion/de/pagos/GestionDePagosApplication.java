944a34a67ea9b2a3e20a798c6597dadab58bb1e8
Vina del Mar <vina@duoc.acad>
2025-05-14 18:03:13 -0400

    ingreso de gestion de pago

---
+++ Gestion-de-pagos/src/main/java/com/GestionDePago/y/ventas/Gestion/de/pagos/GestionDePagosApplication.java
@@ -0,0 +1,13 @@
+package com.GestionDePago.y.ventas.Gestion.de.pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDePagosApplication.class, args);
+	}
+
+}
---
+++ Gestion-de-pagos/src/test/java/com/GestionDePago/y/ventas/Gestion/de/pagos/GestionDePagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionDePago.y.ventas.Gestion.de.pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDePagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
